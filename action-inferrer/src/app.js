78605c2365365c7fd471ee3bca277a76ac9e5ef5
Paulo Martins <paulovmr@gmail.com>
2022-12-01 12:38:01 -0300

    Renamed and added logs to action-inferrer

--- action-inferrer/src/app.js
+++ action-inferrer/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const swaggerFile = require("../openapi.json");
 const app = express();
 const port = 3000;
-const serviceName = "action-predictor";
+const serviceName = "action-inferrer";
 
 app.use(cors());
 
@@ -16,33 +16,40 @@ app.get("/action", (req, res) => {
   // #swagger.operationId = 'getAction'
   const performanceData = {
     numberOfRunningPods: parseInt(req.query.numberOfRunningPods),
-    avgCpuLoad: parseInt(req.query.cpuLoad),
-    avgMemoryUsage: parseInt(req.query.memoryUsage),
+    avgCpuLoad: parseInt(req.query.avgCpuLoad),
+    avgMemoryUsage: parseInt(req.query.avgMemoryUsage),
+  };
+
+  console.info(">> Action inference requested - Pods:", performanceData.numberOfRunningPods, ", CPU:", performanceData.avgCpuLoad + "%", ", Memory:", performanceData.avgMemoryUsage + "%")
+
+  let inference = {
+    action: "NO_ACTION_NEEDED",
+    params: {}
   };
 
   if (performanceData.numberOfRunningPods === 1) {
-    res.send({
+    inference = {
       action: "SCALE_UP",
       params: {
         numberOfPods: 1,
-      },
-    });
+      }
+    };
   } else if (performanceData.numberOfRunningPods === 2) {
-    res.send({
+    inference = {
       action: "SCALE_DOWN",
       params: {
         numberOfPods: 1,
       },
-    });
+    };
   } else if (performanceData.numberOfRunningPods === 3) {
-    res.send({
+    inference = {
       action: "DEQUEUE_USERS",
       params: {
         numberOfUsers: 1,
       },
-    });
+    };
   } else if (performanceData.numberOfRunningPods === 4) {
-    res.send({
+    inference = {
       action: "TRIGGER_WORKFLOW",
       params: {
         workflowId: "workflowId",
@@ -50,12 +57,11 @@ app.get("/action", (req, res) => {
           customParam: "customParam",
         },
       },
-    });
-  } else {
-    res.send({
-      action: "NO_ACTION_NEEDED",
-    });
+    };
   }
+
+  console.info(">> Action inference finished - Action:", inference.action, ", params:", inference.params);
+  res.send(inference);
 });
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
