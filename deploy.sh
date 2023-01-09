f6e8fb71ff4f1fa063eeeec8b284930b9d72773b
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2023-01-09 08:56:02 -0300

    Minor improvements to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # Before using this script:
-# (1) Make sure there are no existing deployments for the services.
-# (2) Update URLs in `src/main/resources/application.properties` if needed.
-# (3) Replace all `caponetto-dev` occurrences with your namespace in the `resources.yaml` file.
-# (4) Export the following environment variables:
+# (1) Update URLs in `serverless-workflow/src/main/resources/application.properties` if needed.
+# (2) Replace all `paulovmr-dev` occurrences with your namespace in the `resources.yaml` file.
+# (3) Export the following environment variables:
 # 	OPENSHIFT_TOKEN
 # 	OPENSHIFT_SERVER
 
 oc login --token=$OPENSHIFT_TOKEN --server=$OPENSHIFT_SERVER
 
-oc apply -f resources.yaml
+APPS=("action-inferrer" "ansible" "monitoring-system" "ticket-manager" "serverless-workflow")
 
-oc start-build action-inferrer
-oc start-build ansible
-oc start-build monitoring-system
-oc start-build ticket-manager
-oc start-build serverless-workflow
+echo -e "\nDeleting old resources..."
+for item in "${APPS[@]}"
+do
+  echo "- $item"
+  oc delete all -l app=$item
+done
+echo "Deleting old resources...done"
+
+echo -e "\nCreating new resources..."
+oc apply -f resources.yaml
+echo "Creating new resources...done"
+
+echo -e "\nStart building resources..."
+for item in "${APPS[@]}"
+do
+  echo "- $item"
+  oc start-build $item
+done
+echo "Start building resources...done"
